3ab7d3c6f6fe00fb7ddec77cdf265216c81b3f7e
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-05-30 10:21:12 -0700

    setted onlicklistener on listview items

--- Listview/app/src/main/java/com/example/listview/MainActivity.java
+++ Listview/app/src/main/java/com/example/listview/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arradapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,samaan );
         mylistview.setAdapter(arradapt);
+
+        mylistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = "Item" + i + "  " + view.toString();
+                Toast.makeText(MainActivity.this,text, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
