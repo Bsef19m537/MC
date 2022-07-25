e0674b9fc652035fbc046111c9b7ed8df064df42
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-07-25 05:07:50 -0700

    Adding arraylist and adapter

--- AdapterandListview/app/src/main/java/com/example/adapterandlistview/MainActivity.java
+++ AdapterandListview/app/src/main/java/com/example/adapterandlistview/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.adapterandlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lvw = (ListView) findViewById(R.id.lv);
+
+        List<String> mylist = new ArrayList<>();
+
+        mylist.add("Class 1 ");
+        mylist.add("Class 2 ");
+        mylist.add("Class 3 ");
+        mylist.add("Class 4 ");
+        mylist.add("Class 5 ");
+        mylist.add("Class 6 ");
+        mylist.add("Class 7 ");
+        mylist.add("Class 8 ");
+        mylist.add("Class 9 ");
+        mylist.add("Class 10");
+
+
+        ArrayAdapter ad = new ArrayAdapter( this,android.R.layout.simple_list_item_1 , mylist);
+
+        lvw.setAdapter(ad);
     }
 }
