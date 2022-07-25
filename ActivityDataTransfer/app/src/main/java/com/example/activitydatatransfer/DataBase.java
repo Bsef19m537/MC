26198f3496ab081fea42f87005c5a5c5bca72c99
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-07-25 00:46:41 -0700

    Activities Of Explicit Intent App Designed

---
+++ ActivityDataTransfer/app/src/main/java/com/example/activitydatatransfer/DataBase.java
@@ -0,0 +1,14 @@
+package com.example.activitydatatransfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataBase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_base);
+    }
+}
---
+++ ActivityDataTransfer/app/src/main/java/com/example/activitydatatransfer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.activitydatatransfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
