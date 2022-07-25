e8dc275b4b4f9b38c33c3b29dbb03d33bfdac3be
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-07-25 01:23:40 -0700

    backend of database activity .. data showingdata

--- ActivityDataTransfer/app/src/main/java/com/example/activitydatatransfer/DataBase.java
+++ ActivityDataTransfer/app/src/main/java/com/example/activitydatatransfer/DataBase.java
@@ -2,7 +2,11 @@ package com.example.activitydatatransfer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DataBase extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class DataBase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_base);
+
+        TextView t1 = (TextView) findViewById(R.id.textView5);
+        TextView t2 = (TextView) findViewById(R.id.textView5);
+        TextView t3 = (TextView) findViewById(R.id.textView5);
+
+        Button btn = (Button) findViewById(R.id.button2);
+
+        Bundle bn = getIntent().getExtras();
+        String name = bn.getString("keyname");
+        String email = bn.getString("keyemail");
+        String id = bn.getString("keyid");
+
+        t1.setText(String.valueOf(name));
+        t2.setText(String.valueOf(email));
+        t3.setText(String.valueOf(id));
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(DataBase.this , MainActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
