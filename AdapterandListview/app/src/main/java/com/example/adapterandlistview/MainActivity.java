20aad920d8b81053af90320e79a924487146c76e
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-07-25 23:28:02 -0700

    adding onitemclick listener on items

--- AdapterandListview/app/src/main/java/com/example/adapterandlistview/MainActivity.java
+++ AdapterandListview/app/src/main/java/com/example/adapterandlistview/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.adapterandlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,22 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvw = (ListView) findViewById(R.id.lv);
 
-        List<String> mylist = new ArrayList<>();
+        ArrayList<String> my = new ArrayList<>();
 
-        mylist.add("Class 1 ");
-        mylist.add("Class 2 ");
-        mylist.add("Class 3 ");
-        mylist.add("Class 4 ");
-        mylist.add("Class 5 ");
-        mylist.add("Class 6 ");
-        mylist.add("Class 7 ");
-        mylist.add("Class 8 ");
-        mylist.add("Class 9 ");
-        mylist.add("Class 10");
+        my.add("Class 1 ");
+        my.add("Class 2 ");
+        my.add("Class 3 ");
+        my.add("Class 4 ");
+        my.add("Class 5 ");
+        my.add("Class 6 ");
+        my.add("Class 7 ");
+        my.add("Class 8 ");
+        my.add("Class 9 ");
+        my.add("Class 10");
 
 
-        ArrayAdapter ad = new ArrayAdapter( this,android.R.layout.simple_list_item_1 , mylist);
+        ArrayAdapter ad = new ArrayAdapter( this,android.R.layout.simple_list_item_1 , my);
 
         lvw.setAdapter(ad);
+
+        lvw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Clicked on " + my(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
