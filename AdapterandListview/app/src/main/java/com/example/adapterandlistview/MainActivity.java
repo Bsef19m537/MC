47243b0a962eb3781d5fd61ceb8819640546bf37
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-07-25 23:33:35 -0700

    adding items through string[] commenting arraylist

--- AdapterandListview/app/src/main/java/com/example/adapterandlistview/MainActivity.java
+++ AdapterandListview/app/src/main/java/com/example/adapterandlistview/MainActivity.java
@@ -22,28 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvw = (ListView) findViewById(R.id.lv);
 
-        ArrayList<String> my = new ArrayList<>();
-
-        my.add("Class 1 ");
-        my.add("Class 2 ");
-        my.add("Class 3 ");
-        my.add("Class 4 ");
-        my.add("Class 5 ");
-        my.add("Class 6 ");
-        my.add("Class 7 ");
-        my.add("Class 8 ");
-        my.add("Class 9 ");
-        my.add("Class 10");
+        String[] items = { "Class 1 " , "Class 2 " , "Class 3 ", "Class 4 " , "Class 5 " , "Class 6 " , "Class 7 " , "Class 8 " , "Class 9 " , "Class 10 " , "Class 11 " , "Class 12 " , "Class 13 " , "Class 14 " , "Class 15 "  };
 
 
-        ArrayAdapter ad = new ArrayAdapter( this,android.R.layout.simple_list_item_1 , my);
+//        ArrayList<String> my = new ArrayList<>();
+//
+//        my.add();
+//        my.add();
+//        my.add();
+//        my.add("Class 4 ");
+//        my.add("Class 5 ");
+//        my.add("Class 6 ");
+//        my.add("Class 7 ");
+//        my.add("Class 8 ");
+//        my.add("Class 9 ");
+//        my.add("Class 10");
+
+
+        ArrayAdapter ad = new ArrayAdapter( this,android.R.layout.simple_list_item_1 , items);
 
         lvw.setAdapter(ad);
 
         lvw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, "Clicked on " + my(i), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Clicked on " + items[i], Toast.LENGTH_SHORT).show();
             }
         });
     }
