fa5761d83d6e008296811297db40d4cd46626d5e
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-08-01 02:40:03 -0700

    creating constructor and gette setter of person class

--- CustomListViewApp/app/src/main/java/com/example/customlistviewapp/person.java
+++ CustomListViewApp/app/src/main/java/com/example/customlistviewapp/person.java
@@ -2,6 +2,38 @@ package com.example.customlistviewapp;
 
 public class person {
 
-    
+    Int image;
+    String name;
+    String des;
 
+    public person(Int image, String name, String des) {
+        this.image = image;
+        this.name = name;
+        this.des = des;
+    }
+
+
+    public Int getImage() {
+        return image;
+    }
+
+    public void setImage(Int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
 }
