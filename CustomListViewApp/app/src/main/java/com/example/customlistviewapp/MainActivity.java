bf26b510d41f963031a70c159b93da696b55f63d
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-08-01 02:36:31 -0700

    creating person data class

--- CustomListViewApp/app/src/main/java/com/example/customlistviewapp/MainActivity.java
+++ CustomListViewApp/app/src/main/java/com/example/customlistviewapp/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.customlistviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = (ListView) findViewById(R.id.list);
+
+        //create data
+
+
     }
 }
