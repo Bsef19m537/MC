53b350a7f081aba50aede45b42dff8eb567750ee
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-08-09 09:10:27 -0700

    designed quiz.xml activity.. added buttons and textview

---
+++ KidsABC/app/src/main/java/com/example/kidsabc/CustomQuiz.java
@@ -0,0 +1,14 @@
+package com.example.kidsabc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_quiz);
+    }
+}
