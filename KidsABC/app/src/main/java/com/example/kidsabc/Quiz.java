9d14ec1e110b7c709705f08eee0119134d748045
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-08-09 09:21:37 -0700

    java code of quiz.class ... setting onclicklistener and intents on buttons

--- KidsABC/app/src/main/java/com/example/kidsabc/Quiz.java
+++ KidsABC/app/src/main/java/com/example/kidsabc/Quiz.java
@@ -2,7 +2,10 @@ package com.example.kidsabc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Quiz extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Button btn1=findViewById(R.id.sq);
+        Button btn2=findViewById(R.id.cq);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent=new Intent(Quiz.this,simplequiz.class);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent=new Intent(Quiz.this,CustomQuiz.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
