5946c2590a77d6da9c74dceed96881210e31ff2b
bsef19m537 <bsef19m537@pucit.edu.pk>
2022-08-09 01:52:02 -0700

    setting url of github repo in implicit intents

--- KidsABC/app/src/main/java/com/example/kidsabc/MainActivity.java
+++ KidsABC/app/src/main/java/com/example/kidsabc/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.kidsabc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_lesson,btn_quiz,btn_repo;
+        btn_lesson=findViewById(R.id.button2);
+        btn_quiz=findViewById(R.id.button3);
+        btn_repo=findViewById(R.id.button);
+
+
+        btn_lesson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,interLesson.class);
+                startActivity(intent);
+            }
+        });
+        btn_quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,interQuiz.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btn_repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/BSEF19M537/MC"));
+                startActivity(browserIntent);
+            }
+        });
+
     }
 }
